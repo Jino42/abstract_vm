079cfe5aa155b8ca1e5dcea89b3f8aaa3b4004b2
Jino <ntoniolo@student.42.fr>
2018-10-22 21:08:46 +0200

    Under Over Flow

---
+++ include/instruction/InstructionException.hpp
@@ -0,0 +1,84 @@
+#ifndef INSTRUCTIONEXCEPTION_HPP
+# define INSTRUCTIONEXCEPTION_HPP
+
+#include <climits>
+#include "IOperand.hpp"
+
+class InstructionException {
+
+public:
+
+	class Underflow : public std::underflow_error {
+	public:
+		Underflow(void) throw();
+		Underflow(std::string) throw();
+		virtual const char* what() const throw();
+		~Underflow(void) throw();
+		Underflow(Underflow const &src) throw();
+	private:
+		Underflow &operator=(Underflow const &rhs) throw();
+		std::string			_error;
+	};
+
+	class Overflow : public std::overflow_error {
+	public:
+		Overflow(void) throw();
+		Overflow(std::string) throw();
+		virtual const char* what() const throw();
+		~Overflow(void) throw();
+		Overflow(Overflow const &src) throw();
+	private:
+		Overflow &operator=(Overflow const &rhs) throw();
+		std::string			_error;
+	};
+
+	class StackTooSmall : public std::logic_error {
+	public:
+		StackTooSmall(void) throw();
+		StackTooSmall(std::string) throw();
+		virtual const char* what() const throw();
+		~StackTooSmall(void) throw();
+		StackTooSmall(StackTooSmall const &src) throw();
+	private:
+		StackTooSmall &operator=(StackTooSmall const &rhs) throw();
+		std::string			_error;
+	};
+
+	class AssertFailed : public std::runtime_error {
+	public:
+		AssertFailed(void) throw();
+		AssertFailed(std::string) throw();
+		virtual const char* what() const throw();
+		~AssertFailed(void) throw();
+		AssertFailed(AssertFailed const &src) throw();
+	private:
+		AssertFailed &operator=(AssertFailed const &rhs) throw();
+		std::string			_error;
+	};
+
+	class DivByZero : public std::domain_error {
+	public:
+		DivByZero(void) throw();
+		DivByZero(std::string) throw();
+		virtual const char* what() const throw();
+		~DivByZero(void) throw();
+		DivByZero(DivByZero const &src) throw();
+	private:
+		DivByZero &operator=(DivByZero const &rhs) throw();
+		std::string			_error;
+	};
+
+	InstructionException(void);
+	~InstructionException(void);
+
+	static void			checkOverflow(eOperandType e, long double value);
+
+
+private:
+	InstructionException(InstructionException const &src);
+	InstructionException		&operator=(InstructionException const &rhs);
+
+	static const bool	_debug;
+};
+
+#endif
---
+++ src/instruction/InstructionException.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include "InstructionException.hpp"
+
+InstructionException::InstructionException(void)
+{
+	if (InstructionException::_debug)
+		std::cout << "InstructionException:: Default constructor called." << std::endl;
+	return ;
+}
+
+InstructionException::~InstructionException(void)
+{
+	if (InstructionException::_debug)
+		std::cout << "InstructionException:: Destructor called." << std::endl;
+	return ;
+}
+
+void					InstructionException::checkOverflow(eOperandType e, long double value)
+{
+	if (e == Int8)
+	{
+		if (value > std::numeric_limits<char>::max())
+			throw(InstructionException::Overflow("Your Operation will make an Int8 Overflow"));
+		if (value < std::numeric_limits<char>::min())
+			throw(InstructionException::Underflow("Your Operation will make an Int8 Underflow"));
+	}
+	else if (e == Int16)
+	{
+		if (value > std::numeric_limits<short>::max())
+			throw(InstructionException::Overflow("Your Operation will make an Int16 Overflow"));
+		if (value < std::numeric_limits<short>::min())
+			throw(InstructionException::Underflow("Your Operation will make an Int16 Underflow"));
+	}
+	else if (e == Int32)
+	{
+		if (value > std::numeric_limits<int>::max())
+			throw(InstructionException::Overflow("Your Operation will make an Int32 Overflow"));
+		if (value < std::numeric_limits<int>::min())
+			throw(InstructionException::Underflow("Your Operation will make an Int32 Underflow"));
+	}
+	else if (e == Float)
+	{
+		if (value > std::numeric_limits<float>::max())
+			throw(InstructionException::Overflow("Your Operation will make an Float Overflow"));
+		if (value < std::numeric_limits<float>::min())
+			throw(InstructionException::Underflow("Your Operation will make an Float Underflow"));
+	}
+	else if (e == Double)
+	{
+		if (value > std::numeric_limits<double>::max())
+			throw(InstructionException::Overflow("Your Operation will make an Double Overflow"));
+		if (value < std::numeric_limits<double>::min())
+			throw(InstructionException::Underflow("Your Operation will make an Double Underflow"));
+	}
+}
+
+
+const bool		InstructionException::_debug = 0;
+
+InstructionException::Underflow::~Underflow(void) throw(){}
+InstructionException::Underflow::Underflow(void) throw() :
+	underflow_error(this->_error),
+	_error("You make a Underflow") {}
+InstructionException::Underflow::Underflow(std::string s) throw() :
+	underflow_error(s),
+	_error(s) { }
+InstructionException::Underflow::Underflow(InstructionException::Underflow const &src) throw() :
+	underflow_error(this->_error),
+	_error(src._error)
+	{ this->_error = src._error; }
+const char	*InstructionException::Underflow::what() const throw()
+	{ return (this->_error.c_str()); }
+
+InstructionException::Overflow::~Overflow(void) throw(){}
+InstructionException::Overflow::Overflow(void) throw() :
+	overflow_error(this->_error),
+	_error("You make a Overflow") {}
+InstructionException::Overflow::Overflow(std::string s) throw() :
+	overflow_error(s),
+	_error(s) { }
+InstructionException::Overflow::Overflow(InstructionException::Overflow const &src) throw() :
+	overflow_error(this->_error),
+	_error(src._error)
+	{ this->_error = src._error; }
+const char	*InstructionException::Overflow::what() const throw()
+	{ return (this->_error.c_str()); }
+
+InstructionException::StackTooSmall::~StackTooSmall(void) throw(){}
+InstructionException::StackTooSmall::StackTooSmall(void) throw() :
+	logic_error(this->_error),
+	_error("Trying operate with a too small stack") {}
+InstructionException::StackTooSmall::StackTooSmall(std::string s) throw() :
+	logic_error(s),
+	_error(s) { }
+InstructionException::StackTooSmall::StackTooSmall(InstructionException::StackTooSmall const &src) throw() :
+	logic_error(this->_error),
+	_error(src._error)
+	{ this->_error = src._error; }
+const char	*InstructionException::StackTooSmall::what() const throw()
+	{ return (this->_error.c_str()); }
+
+InstructionException::DivByZero::~DivByZero(void) throw(){}
+InstructionException::DivByZero::DivByZero(void) throw() :
+	domain_error(this->_error),
+	_error("Trying Divide by 0") {}
+InstructionException::DivByZero::DivByZero(std::string s) throw() :
+	domain_error(s),
+	_error(s) { }
+InstructionException::DivByZero::DivByZero(InstructionException::DivByZero const &src) throw() :
+	domain_error(this->_error),
+	_error(src._error)
+	{ this->_error = src._error; }
+const char	*InstructionException::DivByZero::what() const throw()
+	{ return (this->_error.c_str()); }
+
+
+InstructionException::AssertFailed::~AssertFailed(void) throw(){}
+InstructionException::AssertFailed::AssertFailed(void) throw() :
+	runtime_error(this->_error),
+	_error("Your assert failed") {}
+InstructionException::AssertFailed::AssertFailed(std::string s) throw() :
+	runtime_error(s),
+	_error(s) { }
+InstructionException::AssertFailed::AssertFailed(InstructionException::AssertFailed const &src) throw() :
+	runtime_error(this->_error),
+	_error(src._error)
+	{ this->_error = src._error; }
+const char	*InstructionException::AssertFailed::what() const throw()
+	{ return (this->_error.c_str()); }
